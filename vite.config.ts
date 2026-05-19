c26d82277d44b2ccae034ed47fc9d01007b8c34d
Ahmed Amr <ahmedamr@Ahmeds-MacBook-Pro.local>
2026-05-19 10:50:48 +0300

    Prepare GitHub Pages deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,46 @@
+import { defineConfig } from 'vite'
+import path from 'path'
+import tailwindcss from '@tailwindcss/vite'
+import react from '@vitejs/plugin-react'
+
+
+function figmaAssetResolver() {
+  return {
+    name: 'figma-asset-resolver',
+    resolveId(id) {
+      if (id.startsWith('figma:asset/')) {
+        const filename = id.replace('figma:asset/', '')
+        return path.resolve(__dirname, 'src/assets', filename)
+      }
+    },
+  }
+}
+
 export default defineConfig({
   base: "/DSExams/",
   plugins: [
     figmaAssetResolver(),
+    // The React and Tailwind plugins are both required for Make, even if
+    // Tailwind is not being actively used – do not remove them
     react(),
     tailwindcss(),
   ],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      // Alias @ to the src directory
+      '@': path.resolve(__dirname, './src'),
     },
   },
+<<<<<<< HEAD
   assetsInclude: ["**/*.svg", "**/*.csv"],
 <<<<<<< HEAD
 })
 =======
 })
 >>>>>>> 116cbf5 (Fix GitHub Pages deployment)
+=======
+
+  // File types to support raw imports. Never add .css, .tsx, or .ts files to this.
+  assetsInclude: ['**/*.svg', '**/*.csv'],
+})
+>>>>>>> 9a29767 (Prepare GitHub Pages deploy)
